59a81409779f11a1fc45c3ffd2a588459f452643
Odeesza <samuelmounycosta@gmail.com>
2024-02-21 11:13:51 +0100

    Update flappy.py

--- flappy.py
+++ flappy.py
@@ -64,7 +64,7 @@ class Pipe(pygame.sprite.Sprite):
     def __init__(self, inverted, xpos, ysize):
         pygame.sprite.Sprite.__init__(self)
 
-        self. image = pygame.image.load('assets/sprites/pipe-green.png').convert_alpha()
+        self.image = pygame.image.load('assets/sprites/pipe-green.png').convert_alpha()
         self.image = pygame.transform.scale(self.image, (PIPE_WIDHT, PIPE_HEIGHT))
 
 
@@ -76,7 +76,7 @@ class Pipe(pygame.sprite.Sprite):
             self.rect[1] = - (self.rect[3] - ysize)
         else:
             self.rect[1] = SCREEN_HEIGHT - ysize
-
+        print(self.rect)
 
         self.mask = pygame.mask.from_surface(self.image)
 
